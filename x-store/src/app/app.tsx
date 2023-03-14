4b401093b16f58cb4bb5bce9aa1733c4a39007bb
sergJess <serg.fantasy@yandex.ru>
2023-03-14 14:45:17 +0300

    feat: connect base styles

--- x-store/src/app/app.tsx
+++ x-store/src/app/app.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { MainPage } from '../pages/main-page/main-page';
 import { AboutUsPage } from '../pages/about-us-page/about-us-page';
 import { Page404 } from '../pages/404-page/404-page';
-
+import './styles/base-styles.css';
 export class App extends React.Component {
   render() {
     return (
