a1f1cdc542ab8fd8b0d69831582123e1e8b3956a
sergJess <serg.fantasy@yandex.ru>
2023-03-17 21:01:44 +0300

    refactor: card looks better

--- x-store/vite.config.ts
+++ x-store/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 18080,
+  },
+  test: {
+    globals: true,
+    environment: 'x-store',
   },
 });
