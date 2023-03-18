2b15e9ccb824d4fdee8edbbe78d7aa83be9aec68
sergJess <serg.fantasy@yandex.ru>
2023-03-18 22:09:03 +0300

    feat: add plugin eslint for vite

--- x-store/vite.config.ts
+++ x-store/vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     port: 18080,
   },
