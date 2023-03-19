fede1c4d6624b7617313380da11631252396e7cc
sergJess <serg.fantasy@yandex.ru>
2023-03-19 21:09:07 +0300

    feat: add logo

--- x-store/src/components/header/header.tsx
+++ x-store/src/components/header/header.tsx
@@ -7,6 +7,7 @@ export class Header extends React.Component {
     return (
       <>
         <header className="header">
+          <div className="header__logo">Store</div>
           <Nav />
         </header>
       </>
