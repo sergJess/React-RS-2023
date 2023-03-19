c111baab1d0dea84256042da82774b9daf3b5710
sergJess <serg.fantasy@yandex.ru>
2023-03-19 20:16:49 +0300

    feat: add remote adding data and prettier formatting

--- x-store/src/components/card-container/card-container.tsx
+++ x-store/src/components/card-container/card-container.tsx
@@ -2,30 +2,69 @@ import React from 'react';
 import './card-container.css';
 import { CardInfo, TCardInfoProps } from '../card-info/card-info';
 type TCardContainerProps = {
-  cards: TCardInfoProps[];
+  cards: TCardInfoProps[] | Promise<TCardInfoProps[]>;
 };
-export class CardContainer extends React.Component<TCardContainerProps> {
+type TCardContainerState = {
+  cards?: TCardInfoProps[];
+};
+export class CardContainer extends React.Component<TCardContainerProps, TCardContainerState> {
   constructor(props: TCardContainerProps) {
     super(props);
+    this.state = { cards: [] };
   }
+
+  fillRemoveCards() {
+    if (!Array.isArray(this.props.cards)) {
+      this.props.cards.then((cards) => {
+        this.setState({ cards: cards });
+      });
+    }
+  }
+  componentDidMount() {
+    this.fillRemoveCards();
+  }
+
   render() {
-    return (
-      <div className="card-container">
-        {this.props.cards.map((item) => {
-          return (
-            <CardInfo
-              key={item.id}
-              description={item.description}
-              dislikes={item.dislikes}
-              likes={item.likes}
-              imgSrc={item.imgSrc}
-              priceDollar={item.priceDollar}
-              title={item.title}
-              id={item.id}
-            />
-          );
-        })}
-      </div>
-    );
+    if (Array.isArray(this.props.cards)) {
+      return (
+        <div className="card-container">
+          {this.props.cards.map((item) => {
+            return (
+              <CardInfo
+                key={item.id}
+                description={item.description}
+                dislikes={item.dislikes}
+                likes={item.likes}
+                imgSrc={item.imgSrc}
+                priceDollar={item.priceDollar}
+                title={item.title}
+                id={item.id}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+    if (this.state.cards) {
+      return (
+        <div className="card-container">
+          {this.state.cards.map((item) => {
+            return (
+              <CardInfo
+                key={item.id}
+                description={item.description}
+                dislikes={item.dislikes}
+                likes={item.likes}
+                imgSrc={item.imgSrc}
+                priceDollar={item.priceDollar}
+                title={item.title}
+                id={item.id}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+    return <div className="card-container"></div>;
   }
 }
