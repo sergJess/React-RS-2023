fc934c23ec28a9cef19a3d445a3853363d94be33
sergJess <serg.fantasy@yandex.ru>
2023-03-20 10:06:02 +0300

    feat: add page contact us where form will be rendered

---
+++ x-store/src/pages/contact-us/contact-us.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './contact-us.css';
+export class ContactUs extends React.Component {
+  render() {
+    return (
+      <div className="contact-us" role="contact-us">
+        <h2 className="contact-us__title">Please touch the contact with Us</h2>
+      </div>
+    );
+  }
+}
