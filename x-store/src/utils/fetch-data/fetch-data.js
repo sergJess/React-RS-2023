d5efa407bd238f93b80b767482ef7691b1116d72
sergJess <serg.fantasy@yandex.ru>
2023-03-20 09:47:57 +0300

    fix: fix middlware tsc removes temp js files creteated by tsc and add dist folder to git ignore, removes useless files

--- x-store/src/utils/fetch-data/fetch-data.js
+++
@@ -1,8 +0,0 @@
-export async function fetchData(url) {
-  try {
-    const response = await fetch(url);
-    return await response.json();
-  } catch {
-    throw new Error(`Failed to load data from ${url}`);
-  }
-}
