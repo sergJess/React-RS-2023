7ac2295e18414dba7246a5945df02ef8a261e770
sergJess <serg.fantasy@yandex.ru>
2023-03-20 10:25:28 +0300

    feat: add form

---
+++ x-store/src/components/form-contact/form-contact.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './form-contact.css';
+export class FormContact extends React.Component {
+  render() {
+    return <form className="form-contact"></form>;
+  }
+}
--- x-store/src/pages/contact-us/contact-us.tsx
+++ x-store/src/pages/contact-us/contact-us.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './contact-us.css';
+import { FormContact } from '../../components/form-contact/form-contact';
 export class ContactUs extends React.Component {
   render() {
     return (
       <div className="contact-us" role="contact-us">
         <h2 className="contact-us__title">Please touch the contact with Us</h2>
+        <div className="contact-us__form-wrapper">
+          <FormContact />
+        </div>
       </div>
     );
   }
