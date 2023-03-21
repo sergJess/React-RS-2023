7516cbe98705e93405ac290e759e2d8b6d8c76be
sergJess <serg.fantasy@yandex.ru>
2023-03-21 21:20:17 +0300

    feat: add new styles for contact card

--- x-store/src/components/contact-card/contact-card.tsx
+++ x-store/src/components/contact-card/contact-card.tsx
@@ -11,8 +11,11 @@ export class ContactCard extends React.Component<TContactCardProps> {
   render() {
     return (
       <div className="contact-card">
-        <h3>{this.props.name}</h3>
-        <p>{this.props.surname}</p>
+        <h2 className="contact-card__title">Data from Form-contact</h2>
+        <div className="contact-card__data">
+          <div>{this.props.name}</div>
+          <div>{this.props.surname}</div>
+        </div>
       </div>
     );
   }
