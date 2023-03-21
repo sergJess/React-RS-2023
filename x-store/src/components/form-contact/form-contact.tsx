c1ce91a741ca7c631a64af9bd24c503c91eb1bc6
sergJess <serg.fantasy@yandex.ru>
2023-03-21 20:30:11 +0300

    feat: add form elements

--- x-store/src/components/form-contact/form-contact.tsx
+++ x-store/src/components/form-contact/form-contact.tsx
@@ -13,12 +13,14 @@ export class FormContact extends React.Component<TFormContactProps, TFormContact
   private form: React.RefObject<HTMLFormElement>;
   private inputNameRef: React.RefObject<HTMLInputElement>;
   private inputSurnameRef: React.RefObject<HTMLInputElement>;
+  private inputDateRef: React.RefObject<HTMLInputElement>;
   constructor(props: TFormContactProps) {
     super(props);
     this.submitBtn = this.submitBtn.bind(this);
     this.form = React.createRef();
     this.inputNameRef = React.createRef();
     this.inputSurnameRef = React.createRef();
+    this.inputDateRef = React.createRef();
     this.setDataInvisible = this.setDataInvisible.bind(this);
     this.state = { name: '', surname: '', isDataOk: false };
   }
@@ -28,7 +30,6 @@ export class FormContact extends React.Component<TFormContactProps, TFormContact
     if (testString.length == 0) return false;
     return testString.replace(reg, '').length == 0 ? true : false;
   }
-  componentDidUpdate(): void {}
   submitBtn(e: React.MouseEvent) {
     e.preventDefault();
     const name = this.inputNameRef.current;
@@ -53,19 +54,19 @@ export class FormContact extends React.Component<TFormContactProps, TFormContact
     return (
       <form className="form-contact" ref={this.form}>
         <div className="form-contact__block">
-          <label className="form-contact__text" htmlFor="form-contact-name form-contact__text">
+          <label className="form-contact__text" htmlFor="form-contact-name">
             Type your name:
           </label>
           <input
             className="form-contact__input-text"
-            id="form-contact-name"
+            name="form-contact-name"
             type="text"
             ref={this.inputNameRef}
             placeholder="type here"
           />
         </div>
         <div className="form-contact__block">
-          <label className="form-contact__text" htmlFor="form-contact-surname form-contact__text">
+          <label className="form-contact__text" htmlFor="form-contact-surname">
             Type your surname:
           </label>
           <input
@@ -75,6 +76,64 @@ export class FormContact extends React.Component<TFormContactProps, TFormContact
             ref={this.inputSurnameRef}
             placeholder="type here"
           />
+        </div>
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-date">
+            Select your birthday date:
+          </label>
+          <input
+            name="form-contact-date"
+            className="form-contact__input-text"
+            type="date"
+            id="form-contact-date"
+            ref={this.inputDateRef}
+          />
+        </div>
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-radio">
+            Do You want to receive notification about discount promotions,sales, etc:
+          </label>
+          <span className="form-contact__text">
+            Yep
+            <input
+              className="form-contact__input-text"
+              type="radio"
+              name="form-contact-radio"
+              value="Yep"
+            />
+          </span>
+          <span className="form-contact__text">
+            Nope
+            <input
+              className="form-contact__input-text"
+              type="radio"
+              name="form-contact-radio"
+              value="Nope"
+            />
+          </span>
+        </div>
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-estimate">
+            How can you estimate our store:
+          </label>
+          <select name="form-contact-estimate" defaultValue="select estimate">
+            <option value="Excellent">Excellent</option>
+            <option value="Good">Good</option>
+            <option value="Bad">Bad</option>
+            <option value="Awful">Awful</option>
+          </select>
+        </div>
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-name">
+            Upload your photo or picture:
+          </label>
+          <input className="form-contact__input-text" type="file" />
+        </div>
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-agreement">
+            I consent to my personal data:
+          </label>
+          <input name="form-contact-agreement" type="checkbox" />
         </div>
         <button onClick={this.submitBtn}>Create card</button>
         <div
