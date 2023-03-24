94d96a047c5ba8829e78c472ed0f02e1c15fb2ef
sergJess <serg.fantasy@yandex.ru>
2023-03-24 12:03:30 +0300

    feat: add tests for validate personal

---
+++ x-store/src/utils/validate/validate-personal/validate-personal.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect } from 'vitest';
+import { validatePersonal } from './validate-personal';
+describe('correct validate', () => {
+  test('validate string', () => {
+    const testString = 'Jessie';
+    expect(validatePersonal(testString)).toBe(true);
+  });
+  test('incorrect string', () => {
+    const testString = 'Jessie1997';
+    expect(validatePersonal(testString)).not.toBe(true);
+  });
+  test('correct string with spaces in the end', () => {
+    const testString = 'Jess   ';
+    expect(validatePersonal(testString)).toBe(true);
+  });
+});
