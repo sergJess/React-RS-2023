3e3bf278d850cf5d95005e799c7a46258effa3c3
sergJess <serg.fantasy@yandex.ru>
2023-03-24 12:06:07 +0300

    feat: add new test in validate personal

--- x-store/src/utils/validate/validate-personal/validate-personal.test.ts
+++ x-store/src/utils/validate/validate-personal/validate-personal.test.ts
@@ -9,6 +9,10 @@ describe('correct validate', () => {
     const testString = 'Jessie1997';
     expect(validatePersonal(testString)).not.toBe(true);
   });
+  test('incorrect string because of first letter', () => {
+    const testString = 'jessie';
+    expect(validatePersonal(testString)).not.toBe(true);
+  });
   test('correct string with spaces in the end', () => {
     const testString = 'Jess   ';
     expect(validatePersonal(testString)).toBe(true);
