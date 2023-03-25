518e183eea79574e3f72b42c3f0646f9d80b1098
sergJess <serg.fantasy@yandex.ru>
2023-03-25 12:11:13 +0300

    feat: add tests for contact card

---
+++ x-store/src/components/contact-card/contact-card.test.tsx
@@ -0,0 +1,23 @@
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { ContactCard } from './contact-card';
+describe('component <СontactCard', () => {
+  test('component renders', () => {
+    const component = render(
+      <ContactCard
+        name="Jessie"
+        surname="Rainstone"
+        fileUrl=""
+        estimate="Good"
+        radio="Yep"
+        date="2000-11-11"
+      />
+    );
+    expect(component).toBeTruthy();
+    const name = screen.getByText(/jessie/i);
+    expect(name).toBeTruthy();
+    const estimate = screen.getByText(/good/i);
+    expect(estimate).toBeTruthy();
+  });
+});
