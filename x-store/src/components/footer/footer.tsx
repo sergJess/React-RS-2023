b33fc72808020683ea9bcfff4d90f872b2c9e9c8
sergJess <serg.fantasy@yandex.ru>
2023-03-27 20:53:07 +0300

    refactor: refactoring footer component to FC

--- x-store/src/components/footer/footer.tsx
+++ x-store/src/components/footer/footer.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import './footer.css';
 import cyber from '../../assets/footer/miw-cyber.jpg';
 
-export class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="footer-block">
-          <div>
-            <img src={cyber} alt="cyber" />
-          </div>
-          <div>Guilty pleasures... cyber-x</div>
-          <div>React-2023</div>
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-block">
+        <div>
+          <img src={cyber} alt="cyber" />
         </div>
-      </footer>
-    );
-  }
-}
+        <div>Guilty pleasures... cyber-x</div>
+        <div>React-2023</div>
+      </div>
+    </footer>
+  );
+};
