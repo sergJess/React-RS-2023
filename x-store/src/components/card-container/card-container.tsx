95ce8673d9f22cf7d1aee92e0e2e2201afd432bc
sergJess <serg.fantasy@yandex.ru>
2023-03-27 21:28:56 +0300

    refactor: refactoring component card container to FC in the main page

--- x-store/src/components/card-container/card-container.tsx
+++ x-store/src/components/card-container/card-container.tsx
@@ -1,48 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './card-container.css';
 import { CardInfo, TCardInfoProps } from '../card-info/card-info';
 type TCardContainerProps = {
   cards: TCardInfoProps[] | Promise<TCardInfoProps[]>;
 };
-type TCardContainerState = {
-  cards: TCardInfoProps[];
-};
-export class CardContainer extends React.Component<TCardContainerProps, TCardContainerState> {
-  constructor(props: TCardContainerProps) {
-    super(props);
-    this.state = { cards: [] };
-  }
-
-  fillRemoteCards() {
-    if (!Array.isArray(this.props.cards)) {
-      this.props.cards.then((cards) => {
-        this.setState({ cards: cards });
+export const CardContainer = (props: TCardContainerProps) => {
+  const cardsArr: TCardInfoProps[] = [];
+  const [cards, setCards] = useState(cardsArr);
+  const fillRemoteCards = () => {
+    if (!Array.isArray(props.cards)) {
+      props.cards.then((cards) => {
+        setCards([...cards]);
       });
     }
-  }
-  componentDidMount() {
-    this.fillRemoteCards();
-  }
-
-  render() {
-    const cards = Array.isArray(this.props.cards) ? this.props.cards : this.state.cards;
-    return (
-      <div className="card-container">
-        {cards.map((item) => {
-          return (
-            <CardInfo
-              key={item.id}
-              description={item.description}
-              dislikes={item.dislikes}
-              likes={item.likes}
-              imgSrc={item.imgSrc}
-              priceDollar={item.priceDollar}
-              title={item.title}
-              id={item.id}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+  };
+  useEffect(() => {
+    fillRemoteCards();
+  });
+  const cardsArray = Array.isArray(props.cards) ? props.cards : cards;
+  return (
+    <div className="card-container">
+      {cardsArray.map((item) => {
+        return (
+          <CardInfo
+            key={item.id}
+            description={item.description}
+            dislikes={item.dislikes}
+            likes={item.likes}
+            imgSrc={item.imgSrc}
+            priceDollar={item.priceDollar}
+            title={item.title}
+            id={item.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
