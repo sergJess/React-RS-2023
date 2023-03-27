6e24e63be631c61475418c53c1b5d91fd476ffeb
sergJess <serg.fantasy@yandex.ru>
2023-03-27 20:51:56 +0300

    refactor: refactoring header component to FC

--- x-store/src/components/header/header.tsx
+++ x-store/src/components/header/header.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { Nav } from '../nav/nav';
 import './header.css';
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <>
-        <header className="header">
-          <div className="header__logo">Store</div>
-          <Nav />
-        </header>
-      </>
-    );
-  }
-}
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo">Store</div>
+        <Nav />
+      </header>
+    </>
+  );
+};
