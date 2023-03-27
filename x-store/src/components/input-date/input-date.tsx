f7c1f6ec3de64009dc25f17c54b8affb2f853854
sergJess <serg.fantasy@yandex.ru>
2023-03-27 20:58:19 +0300

    refactor: refactoring input date component to FC

--- x-store/src/components/input-date/input-date.tsx
+++ x-store/src/components/input-date/input-date.tsx
@@ -7,24 +7,19 @@ export type TInputDate = {
   labelClass: string;
   inputClass: string;
 };
-export class InputDate extends React.Component<TInputDate> {
-  constructor(props: TInputDate) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={this.props.wrapperClass}>
-        <label className={this.props.labelClass} htmlFor={this.props.htmlFor}>
-          {this.props.labelText}
-        </label>
-        <input
-          className={this.props.inputClass}
-          type="date"
-          role="input-date"
-          name={this.props.htmlFor}
-          ref={this.props.inputRef}
-        />
-      </div>
-    );
-  }
-}
+export const InputDate = (props: TInputDate) => {
+  return (
+    <div className={props.wrapperClass}>
+      <label className={props.labelClass} htmlFor={props.htmlFor}>
+        {props.labelText}
+      </label>
+      <input
+        className={props.inputClass}
+        type="date"
+        role="input-date"
+        name={props.htmlFor}
+        ref={props.inputRef}
+      />
+    </div>
+  );
+};
