fa5884986fe9ff1f938e44fe214bbb8dcb717f36
sergJess <serg.fantasy@yandex.ru>
2023-03-28 12:12:07 +0300

    refactor: refactor part of form component to hooks

--- x-store/src/components/form-contact/form-contact.tsx
+++ x-store/src/components/form-contact/form-contact.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './form-contact.css';
 import { TContactCardProps } from '../contact-card/contact-card';
 import { InputText } from '../input-text/input-text';
@@ -38,7 +38,271 @@ type TFormDatas = {
   date: string;
   agreement: HTMLInputElement;
 };
-export class FormContact extends React.Component<TFormContactProps, TFormContactState> {
+export const FormContact = (props: TFormContactProps) => {
+  const form = useRef<HTMLFormElement | null>(null);
+  const inputNameRef = useRef<HTMLInputElement | null>(null);
+  const inputSurnameRef = useRef<HTMLInputElement | null>(null);
+  const inputEmailRef = useRef<HTMLInputElement | null>(null);
+  const inputDateRef = useRef<HTMLInputElement | null>(null);
+  const inputRadioFirstRef = useRef<HTMLInputElement | null>(null);
+  const inputRadioSecondRef = useRef<HTMLInputElement | null>(null);
+  const selectRef = useRef<HTMLSelectElement | null>(null);
+  const checkAgreementRef = useRef<HTMLInputElement | null>(null);
+  const inputFileRef = useRef<HTMLInputElement | null>(null);
+  const initialState: TFormContactState = {
+    cardData: {
+      name: '',
+      surname: '',
+      email: '',
+      date: '',
+      radio: '',
+      estimate: '',
+      fileUrl: '',
+    },
+    validCardDate: {
+      isCorrectName: false,
+      isCorerectSurname: false,
+      isCorrectEmail: false,
+      isCheckedRadio: false,
+      isCorrectDate: false,
+      isEstimated: false,
+      isAgreement: false,
+      isAttachedFile: false,
+    },
+    isDataOk: false,
+    isInitial: true,
+  };
+  const [state, setState] = useState({ ...initialState });
+  return (
+    <form className="form-contact" ref={form}>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isCorrectName
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <InputText
+          wrapperClass="form-contact__block"
+          labelClass="form-contact__text"
+          inputClass="form-contact__input-text"
+          labelText="Type your name:"
+          htmlFor="form-contact-name"
+          role="name"
+          placeholder="type here"
+          inputRef={inputNameRef}
+        />
+        <span
+          className={
+            state.isInitial || state.validCardDate.isCorrectName
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isCorerectSurname
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <InputText
+          wrapperClass="form-contact__block"
+          labelClass="form-contact__text"
+          role="surname"
+          inputClass="form-contact__input-text"
+          labelText="Type your surname:"
+          htmlFor="form-contact-surname"
+          placeholder="type here"
+          inputRef={inputSurnameRef}
+        />
+        <span
+          className={
+            state.isInitial || state.validCardDate.isCorerectSurname
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isCorrectDate
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <InputDate
+          wrapperClass="form-contact__block"
+          labelClass="form-contact__text"
+          inputClass="form-contact__input-text"
+          labelText="Select your birthday date:"
+          htmlFor="form-contact-date"
+          inputRef={inputDateRef}
+        />
+        <span
+          className={
+            state.isInitial || state.validCardDate.isCorrectDate
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isCorrectEmail
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <InputText
+          wrapperClass="form-contact__block"
+          labelClass="form-contact__text"
+          role="email"
+          inputClass="form-contact__input-text"
+          labelText="Type your email:"
+          htmlFor="form-contact-email"
+          placeholder="type here"
+          inputRef={inputEmailRef}
+        />
+        <span
+          className={
+            state.isInitial || state.validCardDate.isCorrectEmail
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isCheckedRadio
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <InputRadio
+          wrapperClass="form-contact__block"
+          labelClass="form-contact__text"
+          labelText=" Do You want to receive notification about discount promotions,sales, etc:"
+          htmlFor="form-contact-radio"
+          radios={[
+            {
+              value: 'Yep',
+              textNearRadioClass: 'form-contact__text',
+              inputRef: inputRadioFirstRef,
+            },
+            {
+              value: 'Nope',
+              textNearRadioClass: 'form-contact__text',
+              inputRef: inputRadioSecondRef,
+            },
+          ]}
+        />
+        <span
+          className={
+            state.isInitial || state.validCardDate.isCheckedRadio
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isEstimated
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <SelectOptions
+          htmlFor="form-contact-estimate"
+          optionValues={['Excellent', 'Good', 'Bad', 'Awful']}
+          selectRef={selectRef}
+          labelText="How can you estimate our store:"
+          wrapperClass="form-contact__block"
+          labelClass="form-contact__text"
+          firstOptionText="Choose estimate:"
+        />
+        <span
+          className={
+            state.isInitial || state.validCardDate.isEstimated
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isAttachedFile
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-name">
+            Upload your photo or picture:
+          </label>
+          <input
+            className="form-contact__input-text"
+            role="input-file"
+            ref={inputFileRef}
+            type="file"
+          />
+        </div>
+        <span
+          className={
+            state.isInitial || state.validCardDate.isAttachedFile
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+      <div
+        className={
+          state.isInitial || state.validCardDate.isAgreement
+            ? 'form-contact__block-warapper'
+            : 'form-contact__block-warapper form-contact__block-warapper_incorrect'
+        }
+      >
+        <div className="form-contact__block">
+          <label className="form-contact__text" htmlFor="form-contact-agreement">
+            I consent to my personal data:
+          </label>
+          <input ref={checkAgreementRef} name="form-contact-agreement" type="checkbox" />
+        </div>
+        <span
+          className={
+            state.isInitial || state.validCardDate.isAgreement
+              ? 'form-contact__incorrect-text form-contact__incorrect_hide'
+              : 'form-contact__incorrect-text form-contact__incorrect_show'
+          }
+        >
+          is not correct
+        </span>
+      </div>
+
+      <button className="form-contact__submit">Create card</button>
+      <div className={state.isDataOk ? 'form-ok' : 'form-ok form-ok_invisible'}>
+        Your data has been safed
+        <button className="form-ok__btn">Ok</button>
+      </div>
+    </form>
+  );
+};
+export class FormContactX extends React.Component<TFormContactProps, TFormContactState> {
   private form: React.RefObject<HTMLFormElement> = React.createRef();
   private inputNameRef: React.RefObject<HTMLInputElement> = React.createRef();
   private inputSurnameRef: React.RefObject<HTMLInputElement> = React.createRef();
