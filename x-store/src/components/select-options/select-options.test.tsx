c71b2bd662d6c3e115b9cb8e4a67997cc96d2bca
sergJess <serg.fantasy@yandex.ru>
2023-04-01 12:15:16 +0300

    fix: fixing tests for select options

--- x-store/src/components/select-options/select-options.test.tsx
+++ x-store/src/components/select-options/select-options.test.tsx
@@ -1,19 +1,26 @@
 import { describe, test, expect } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, renderHook } from '@testing-library/react';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { SelectOptions } from './select-options';
+type TSelectOptionsTest = {
+  select: string;
+};
 describe('component <SelectOptions/>', () => {
   test('component renders', () => {
-    const selectRef: React.RefObject<HTMLSelectElement> = React.createRef();
+    const { result } = renderHook(() => {
+      const { register } = useForm<TSelectOptionsTest>();
+      const naming = { ...register('select', { required: true }) };
+      return naming;
+    });
     const component = render(
       <SelectOptions
-        htmlFor="form-contact-estimate"
         optionValues={['Excellent', 'Good', 'Bad', 'Awful']}
-        selectRef={selectRef}
         labelText="How can you estimate our store:"
         wrapperClass="form-contact__block"
         labelClass="form-contact__text"
         firstOptionText="Choose estimate:"
+        register={result.current}
       />
     );
     expect(component).toBeTruthy();
@@ -21,12 +28,15 @@ describe('component <SelectOptions/>', () => {
     expect(label).toBeTruthy();
   });
   test('select gets value from option', () => {
-    const selectRef: React.RefObject<HTMLSelectElement> = React.createRef();
+    const { result } = renderHook(() => {
+      const { register } = useForm<TSelectOptionsTest>();
+      const naming = { ...register('select', { required: true }) };
+      return naming;
+    });
     const component = render(
       <SelectOptions
-        htmlFor="form-contact-estimate"
+        register={result.current}
         optionValues={['Excellent', 'Good', 'Bad', 'Awful']}
-        selectRef={selectRef}
         labelText="How can you estimate our store:"
         wrapperClass="form-contact__block"
         labelClass="form-contact__text"
@@ -36,13 +46,11 @@ describe('component <SelectOptions/>', () => {
     expect(component).toBeTruthy();
     const options = screen.getAllByRole('option') as HTMLOptionElement[];
     expect(options.length).toBeGreaterThan(0);
-    const select = selectRef.current!;
-    expect(select.value).toMatch('');
-    expect(select.value).toBe(options[0].value);
-    fireEvent.change(select, { target: { value: 'Good' } });
-    expect(select.value).toBe(options[2].value);
     const selectElem = screen.getByRole('combobox') as HTMLSelectElement;
     expect(selectElem).toBeTruthy();
-    expect(selectElem.value).toBe(select.value);
+    expect(selectElem.value).toMatch('');
+    expect(selectElem.value).toBe(options[0].value);
+    fireEvent.change(selectElem, { target: { value: 'Good' } });
+    expect(selectElem.value).toBe(options[2].value);
   });
 });
